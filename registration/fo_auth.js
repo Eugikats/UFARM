f688858b7ef8a7a8a8a5a0a59ea83612368e00ae
Eugene Katusiime <katusiime66@gmail.com>
2023-04-18 15:46:32 +0300

    ADDED THE FOLLOWING Dashboards/Ao_dashboard.html         new file:   Dashboards/Fo_dashboard         new file:   Dashboards/Uf_dashboard.html         new file:   auth/ao_login.html         new file:   auth/customer_login.html         new file:   auth/fo_login.html         modified:   auth/login.html         new file:   auth/register_as.html         new file:   auth/uf_login.html         modified:   index.html         new file:   products/product_upload.html         new file:   registration/AO-registration.html         modified:   registration/UF_registration.html         new file:   registration/customer-registration.html         new file:   registration/customer_auth.js         modified:   registration/fo_auth.js         modified:   registration/register.html         modified:   registration/uf_auth.js

--- registration/fo_auth.js
+++ registration/fo_auth.js
@@ -64,13 +64,28 @@
     let firstName = document.register.firstname
     let lastName = document.register.lastname
     let uniqueno = document.register.uniquenumber
+    let gender = document.register.gender
     let password = document.getElementById("password")
+    let ward = document.register.ward
+    let phone = document.register.phonenumber
+    let address = document.register.address
+    let residence = document.register.residencetype
+   
+
+  
 
     //pick error sections
     let firstNameError = document.getElementById("fnameErr")
     let lastNameError = document.getElementById("lnameErr")
     let uniquenoError = document.getElementById("uniquenoErr")
     let passError = document.getElementById("passErr")
+    let genderError = document.getElementById("genderErr")
+    let wardError = document.getElementById("wardErr")
+    let phoneError = document.getElementById("phoneErr")
+    let addressError = document.getElementById("addressErr")
+    let residenceErr = document.getElementById("residenceErr")
+    
+
 
     // validating first name emptiness
     if(firstName.value==""){
@@ -85,15 +100,18 @@
       firstName.style.border = "1px solid red"
         firstNameError.innerHTML = "Please the first name must be atleast 5 letters"
         firstNameError.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+        firstName.focus()
+        return false;
     }
     // validating firstname max length
     else if(firstName.value.length > 50) {
-      firstName.style.border = "1px solid red"
+      firstName.style.border = "2px solid red"
       firstNameError.innerHTML = "First Name must be below 50 characters"
       firstNameError.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+      firstName.focus()
       return false;
     } else{
-      firstName.style.border = "1px solid green"
+      firstName.style.border = "2px solid green"
       firstNameError.innerHTML = ""
       lastName.focus()
     }
@@ -123,7 +141,35 @@
    else{
     lastName.style.border = "1px solid green"
     lastNameError.innerHTML = ""
-    uniqueno.focus()
+   gender.focus()
+  }
+
+  //gender validation
+  if (gender.value !== "Male" && gender.value !== "Female") {
+    gender.style.border = "1px solid red"
+    genderError.textContent = "Gender is required";
+    genderError.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+
+    return false;
+  }
+  else {
+    gender.style.border = "2px solid green"
+    genderError.textContent="";
+    gender.focus();
+  }
+
+  //ward validation
+  if (ward.value=="") {
+    ward.style.border = "1px solid red"
+    wardError.textContent = "Ward is required";
+    wardError.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+
+    return false;
+  }
+  else {
+    ward.style.border = "2px solid green"
+    wardError.textContent="";
+    uniqueno.focus();
   }
 
     //unique number verifications
@@ -136,14 +182,56 @@
     }
     else if(!(uniqueno.value.match(foregex))){
       uniqueno.style.border = "1px solid red"
-      uniquenoError.textContent = "Unique number must follow this formart AO-000";
+      uniquenoError.textContent = "Unique number must follow this formart FO-000";
       uniquenoError.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
       return false;
     }else {
       uniqueno.style.border = "1px solid green"
       uniquenoError.textContent = "";
-      password.focus()
+      phone.focus()
     }
+
+     //phone number validation
+  if(phone.value == ""){
+    phone.style.border = "2px solid red"
+      phoneError.innerHTML = "Please, phone number can not be empty"
+      phoneError.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+      phone.focus()
+      return false;
+  }
+  else {
+    phone.style.border = "2px solid green"
+    phoneError.textContent="";
+    residence.focus();
+  }
+
+  //residence type validation
+  if(residence.value == ""){
+    residence.style.border = "2px solid red"
+      residenceErr.innerHTML = "Please, residence number can not be empty"
+      residenceErr.style = "color: red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+      residence.focus()
+      return false;
+  }
+  else {
+    residence.style.border = "2px solid green"
+    residenceErr.textContent="";
+    password.focus();
+  }
+
+  // validating address emptiness
+  if (address.value == "") {
+    address.style.border = "2px solid red"
+      addressError.innerHTML = "Please, address number can not be empty"
+      addressError.style = "color:red; font-size:11px; font-family:Arial, Helvetica, sans-serif;";
+      address.focus()
+      return false;
+  }
+  else {
+    address.style.border = "1px solid green"
+    addressError.innerHTML = ""
+    // password.focus()
+  }
 
     //password validation
     const passregex = /^[A-Za-z]\w{7,14}$/;
